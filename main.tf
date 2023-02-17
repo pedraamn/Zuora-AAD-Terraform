1ebf4bbc4d99b2a269a2dbdc01afeb5f0247ff5d
Orion Kimenker <118377802+okimenker-zuora@users.noreply.github.com>
2023-02-16 21:35:50 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-variable "azure_domain"{}
-variable "users_file_path"{}
-variable "managers_file_path"{}
-variable "groups_file_path"{}
-variable "existing_groups_file_path"{}
-variable "sam_object_id"{}
-variable "pedraam_object_id"{}
-variable "dan_object_id"{}
-
 module "app1" {
     source  = "./enterprise_app"
     azure_domain = var.azure_domain
